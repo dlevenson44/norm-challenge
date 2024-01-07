3b7cbb72a51a4340aa89d9cc146327c36746c183
dlevenson44 <daniellevenson4@gmail.com>
2024-01-07 18:03:28 -0500

    add loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Card from '@mui/material/Card'
+import CircularProgress from '@mui/material/CircularProgress'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import isEmpty from 'lodash/isEmpty'
@@ -13,24 +14,31 @@ import { fetcher } from './helpers'
 import { FETCH_URL, REFRESH_INTERVAL } from './utils'
 
 function App() {
- const { data, error, isLoading } = useSWR(
+ const { data, error } = useSWR(
   FETCH_URL,
   fetcher,
   { refreshInterval: REFRESH_INTERVAL }
  )
 
+ const isLoading = true
  const rootChildren = React.useMemo(() => data?.tree?.children, [data?.tree?.children])
 
  return (
-  <Stack spacing={3} alignItems="center">
-   {!isLoading && !isEmpty(rootChildren) ?
+  <Stack height="100vh" spacing={3} alignItems="center" justifyContent="center">
+   {isLoading && (
+    <Stack alignItems="center">
+     <CircularProgress />
+    </Stack>
+   )}
+   {!isLoading && !isEmpty(rootChildren) &&
     map(rootChildren, (checkNode, idx) => (
      <Card key={idx} raised sx={{ width: '75%', p: 2 }}>
       <CheckNode {...checkNode} />
      </Card>
-    )) : (
-     <Typography variant="h4">No Data to Render!</Typography>
-    )}
+    ))}
+   {!isLoading && isEmpty(rootChildren) &&
+			<Typography variant="h4">No Data to Render!</Typography>
+   }
    {!isLoading && error && <Typography variant="h4">Error Fetching Data!</Typography>}
   </Stack>
  )
